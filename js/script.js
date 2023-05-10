2edcaf6472f9d02f3da59eda1e29d398cc8fd9a3
AirDevil188 <urosculibrk@ymail.comgit config --global init.defaultBranch maingit config --global color.ui autogit config --global pull.rebase false[200~git config --get user.namegit config --get user.namegit config --get user.emailcleargit config --global user.email urosculibrk@ymail.com>
2023-05-10 22:18:57 +0200

    added function for adding books object to array

--- js/script.js
+++ js/script.js
@@ -6,3 +6,12 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
 }
+
+function addBookToArray() {
+  let title = document.getElementById("book_title").value;
+  let author = document.getElementById("book_author").value;
+  let pages = document.getElementById("number_of_pages").value;
+  let read = document.getElementById("read").checked;
+  let newBook = new Book(title, author, pages, read);
+  return myLibrary.push(newBook);
+}
